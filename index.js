d1df67b6a38cb06b58acdf34c3d9d17cdbd20478
Christopher Rokita <chris@livio.io>
2017-06-14 09:37:09 -0400

    Revert previous commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ module.exports = function (ip) {
 function getSetCheck (key, callback) {
 	var modIndex;
 	consul.kv.get(key, function (err, res) {
-		if (!err) {
+		if (res) {
 			modIndex = res.ModifyIndex;
 		}
 		callback(res, setter);
